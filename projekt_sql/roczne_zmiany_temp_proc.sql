0f2de9f8753409462a3309745b99932b1e7e4341
pdawic <dawicki@gmail.com>
2019-11-23 12:57:44 +0100

    cleaning

--- projekt_sql/roczne_zmiany_temp_proc.sql
+++ projekt_sql/roczne_zmiany_temp_proc.sql
@@ -1,26 +1,27 @@
---roczne zmiany temperatur w procentach
+--miesieczne zmiany temperatur w procentach
 --San Francisco
 --Palo Alto
 --Redwood City
 --San Jose
 --Mountain View
 
+
+
 with wsad as
          (
              select  distinct ztc.city city,
                               wn.rok rok,
                               wn.miesiac miesiac,
-                              avg(wn.min_temperature_c) over (partition by ztc.city,wn.rok) avg_min_temp_rok,
-                              avg(wn.max_temerature_c) over (partition by ztc.city,wn.rok)   avg_max_temp_rok,
-                              avg(wn.mean_temperature_c) over (partition by ztc.city,wn.rok) avg_mean_temp_rok,
-                              avg(wn.min_humidity) over (partition by ztc.city,wn.rok) avg_min_humidity_rok,
-                              avg(wn.max_humidity) over (partition by ztc.city,wn.rok) avg_max_humidity_rok,
-                              avg(wn.mean_humidity) over (partition by ztc.city,wn.rok) avg_mean_humidity_rok,
-                              avg(wn.mean_wind_speed_mph) over (partition by ztc.city,wn.rok) avg_mean_wind_speed_mph_rok,
-                              avg(wn.max_wind_speed_mph) over (partition by ztc.city,wn.rok) avg_max_wind_speed_mph_rok
+                              avg(wn.min_temperature_c) over (partition by ztc.city,wn.rok,wn.miesiac) avg_min_temp_month,
+                              avg(wn.max_temerature_c) over (partition by ztc.city,wn.rok,wn.miesiac)   avg_max_temp_month,
+                              avg(wn.mean_temperature_c) over (partition by ztc.city,wn.rok,wn.miesiac) avg_mean_temp_month,
+                              avg(wn.min_humidity) over (partition by ztc.city,wn.rok,wn.miesiac) avg_min_humidity_month,
+                              avg(wn.max_humidity) over (partition by ztc.city,wn.rok,wn.miesiac) avg_max_humidity_month,
+                              avg(wn.mean_humidity) over (partition by ztc.city,wn.rok,wn.miesiac) avg_mean_humidity_month,
+                              avg(wn.mean_wind_speed_mph) over (partition by ztc.city,wn.rok,wn.miesiac) avg_mean_wind_speed_mph,
+                              avg(wn.max_wind_speed_mph) over (partition by ztc.city,wn.rok,wn.miesiac) avg_max_wind_speed_mph
              from weather_norm wn
                       join zip_to_cities ztc on wn.zip_code = ztc.zip_code
-             --where ztc.city = 'Mountain View'
              group by ztc.city,wn.rok, wn.miesiac,
                       wn.min_temperature_C, wn.max_temerature_C, wn.mean_temperature_C,
                       wn.min_humidity, wn.max_humidity, wn.mean_humidity,
@@ -31,119 +32,124 @@ with wsad as
 select
        city,
        rok,
-       avg_min_temp_rok,
-       (((avg_min_temp_rok - lag(avg_min_temp_rok) over ())/lag(avg_min_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_min_temp_rok,
-       avg_max_temp_rok,
-       (((avg_max_temp_rok - lag(avg_max_temp_rok) over ())/lag(avg_max_temp_rok) over ())*100)::numeric(3)  yoy_proc_avg_max_temp_rok,
-       avg_mean_temp_rok,
-       (((avg_mean_temp_rok - lag(avg_mean_temp_rok) over ())/lag(avg_mean_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_temp_rok,
-       avg_min_humidity_rok,
-       (((avg_min_humidity_rok - lag(avg_min_humidity_rok) over ())/lag(avg_min_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_min_humidity_rok,
-       avg_max_humidity_rok,
-       (((avg_max_humidity_rok - lag(avg_max_humidity_rok) over ())/lag(avg_max_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_max_humidity_rok,
-       avg_mean_humidity_rok,
-       (((avg_mean_humidity_rok - lag(avg_mean_humidity_rok) over ())/lag(avg_mean_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_humidity_rok,
-       avg_mean_wind_speed_mph_rok,
-       (((avg_mean_wind_speed_mph_rok - lag(avg_mean_wind_speed_mph_rok) over ())/lag(avg_mean_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_wind_speed_mph_rok,
-       avg_max_wind_speed_mph_rok,
-       (((avg_max_wind_speed_mph_rok - lag(avg_max_wind_speed_mph_rok) over ())/lag(avg_max_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_max_wind_speed_mph_rok
+       miesiac,
+       avg_min_temp_month,
+       (((avg_min_temp_month - lag(avg_min_temp_month) over ())/lag(avg_min_temp_month) over ())*100)::numeric(3) MoM_proc_avg_min_temp_month,
+       avg_max_temp_month,
+       (((avg_max_temp_month - lag(avg_max_temp_month) over ())/lag(avg_max_temp_month) over ())*100)::numeric(3)  MoM_proc_avg_max_temp_month,
+       avg_mean_temp_month,
+       (((avg_mean_temp_month - lag(avg_mean_temp_month) over ())/lag(avg_mean_temp_month) over ())*100)::numeric(3) MoM_proc_avg_mean_temp_month,
+       avg_min_humidity_month,
+       (((avg_min_humidity_month - lag(avg_min_humidity_month) over ())/lag(avg_min_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_min_humidity_month,
+       avg_max_humidity_month,
+       (((avg_max_humidity_month - lag(avg_max_humidity_month) over ())/lag(avg_max_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_max_humidity_month,
+       avg_mean_humidity_month,
+       (((avg_mean_humidity_month - lag(avg_mean_humidity_month) over ())/lag(avg_mean_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_mean_humidity_month,
+       avg_mean_wind_speed_mph,
+       (((avg_mean_wind_speed_mph - lag(avg_mean_wind_speed_mph) over ())/lag(avg_mean_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_mean_wind_speed_mph,
+       avg_max_wind_speed_mph,
+       (((avg_max_wind_speed_mph - lag(avg_max_wind_speed_mph) over ())/lag(avg_max_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_max_wind_speed_mph
 
 from wsad
-where city = 'Mountain View'
-group by 1,2,3,5,7,9,11,13,15,17
+where wsad.city = 'Mountain View'
+group by 1,2,3,4,6,8,10,12,14,16,18
 union all
 select
        city,
        rok,
-       avg_min_temp_rok,
-       (((avg_min_temp_rok - lag(avg_min_temp_rok) over ())/lag(avg_min_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_min_temp_rok,
-       avg_max_temp_rok,
-       (((avg_max_temp_rok - lag(avg_max_temp_rok) over ())/lag(avg_max_temp_rok) over ())*100)::numeric(3)  yoy_proc_avg_max_temp_rok,
-       avg_mean_temp_rok,
-       (((avg_mean_temp_rok - lag(avg_mean_temp_rok) over ())/lag(avg_mean_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_temp_rok,
-       avg_min_humidity_rok,
-       (((avg_min_humidity_rok - lag(avg_min_humidity_rok) over ())/lag(avg_min_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_min_humidity_rok,
-       avg_max_humidity_rok,
-       (((avg_max_humidity_rok - lag(avg_max_humidity_rok) over ())/lag(avg_max_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_max_humidity_rok,
-       avg_mean_humidity_rok,
-       (((avg_mean_humidity_rok - lag(avg_mean_humidity_rok) over ())/lag(avg_mean_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_humidity_rok,
-       avg_mean_wind_speed_mph_rok,
-       (((avg_mean_wind_speed_mph_rok - lag(avg_mean_wind_speed_mph_rok) over ())/lag(avg_mean_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_wind_speed_mph_rok,
-       avg_max_wind_speed_mph_rok,
-       (((avg_max_wind_speed_mph_rok - lag(avg_max_wind_speed_mph_rok) over ())/lag(avg_max_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_max_wind_speed_mph_rok
+       miesiac,
+       avg_min_temp_month,
+       (((avg_min_temp_month - lag(avg_min_temp_month) over ())/lag(avg_min_temp_month) over ())*100)::numeric(3) MoM_proc_avg_min_temp_month,
+       avg_max_temp_month,
+       (((avg_max_temp_month - lag(avg_max_temp_month) over ())/lag(avg_max_temp_month) over ())*100)::numeric(3)  MoM_proc_avg_max_temp_month,
+       avg_mean_temp_month,
+       (((avg_mean_temp_month - lag(avg_mean_temp_month) over ())/lag(avg_mean_temp_month) over ())*100)::numeric(3) MoM_proc_avg_mean_temp_month,
+       avg_min_humidity_month,
+       (((avg_min_humidity_month - lag(avg_min_humidity_month) over ())/lag(avg_min_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_min_humidity_month,
+       avg_max_humidity_month,
+       (((avg_max_humidity_month - lag(avg_max_humidity_month) over ())/lag(avg_max_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_max_humidity_month,
+       avg_mean_humidity_month,
+       (((avg_mean_humidity_month - lag(avg_mean_humidity_month) over ())/lag(avg_mean_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_mean_humidity_month,
+       avg_mean_wind_speed_mph,
+       (((avg_mean_wind_speed_mph - lag(avg_mean_wind_speed_mph) over ())/lag(avg_mean_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_mean_wind_speed_mph,
+       avg_max_wind_speed_mph,
+       (((avg_max_wind_speed_mph - lag(avg_max_wind_speed_mph) over ())/lag(avg_max_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_max_wind_speed_mph
 
 from wsad
-where city = 'Palo Alto'
-group by 1,2,3,5,7,9,11,13,15,17
+where wsad.city = 'San Francisco'
+group by 1,2,3,4,6,8,10,12,14,16,18
 union all
 select
        city,
        rok,
-       avg_min_temp_rok,
-       (((avg_min_temp_rok - lag(avg_min_temp_rok) over ())/lag(avg_min_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_min_temp_rok,
-       avg_max_temp_rok,
-       (((avg_max_temp_rok - lag(avg_max_temp_rok) over ())/lag(avg_max_temp_rok) over ())*100)::numeric(3)  yoy_proc_avg_max_temp_rok,
-       avg_mean_temp_rok,
-       (((avg_mean_temp_rok - lag(avg_mean_temp_rok) over ())/lag(avg_mean_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_temp_rok,
-       avg_min_humidity_rok,
-       (((avg_min_humidity_rok - lag(avg_min_humidity_rok) over ())/lag(avg_min_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_min_humidity_rok,
-       avg_max_humidity_rok,
-       (((avg_max_humidity_rok - lag(avg_max_humidity_rok) over ())/lag(avg_max_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_max_humidity_rok,
-       avg_mean_humidity_rok,
-       (((avg_mean_humidity_rok - lag(avg_mean_humidity_rok) over ())/lag(avg_mean_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_humidity_rok,
-       avg_mean_wind_speed_mph_rok,
-       (((avg_mean_wind_speed_mph_rok - lag(avg_mean_wind_speed_mph_rok) over ())/lag(avg_mean_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_wind_speed_mph_rok,
-       avg_max_wind_speed_mph_rok,
-       (((avg_max_wind_speed_mph_rok - lag(avg_max_wind_speed_mph_rok) over ())/lag(avg_max_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_max_wind_speed_mph_rok
+       miesiac,
+       avg_min_temp_month,
+       (((avg_min_temp_month - lag(avg_min_temp_month) over ())/lag(avg_min_temp_month) over ())*100)::numeric(3) MoM_proc_avg_min_temp_month,
+       avg_max_temp_month,
+       (((avg_max_temp_month - lag(avg_max_temp_month) over ())/lag(avg_max_temp_month) over ())*100)::numeric(3)  MoM_proc_avg_max_temp_month,
+       avg_mean_temp_month,
+       (((avg_mean_temp_month - lag(avg_mean_temp_month) over ())/lag(avg_mean_temp_month) over ())*100)::numeric(3) MoM_proc_avg_mean_temp_month,
+       avg_min_humidity_month,
+       (((avg_min_humidity_month - lag(avg_min_humidity_month) over ())/lag(avg_min_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_min_humidity_month,
+       avg_max_humidity_month,
+       (((avg_max_humidity_month - lag(avg_max_humidity_month) over ())/lag(avg_max_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_max_humidity_month,
+       avg_mean_humidity_month,
+       (((avg_mean_humidity_month - lag(avg_mean_humidity_month) over ())/lag(avg_mean_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_mean_humidity_month,
+       avg_mean_wind_speed_mph,
+       (((avg_mean_wind_speed_mph - lag(avg_mean_wind_speed_mph) over ())/lag(avg_mean_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_mean_wind_speed_mph,
+       avg_max_wind_speed_mph,
+       (((avg_max_wind_speed_mph - lag(avg_max_wind_speed_mph) over ())/lag(avg_max_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_max_wind_speed_mph
 
 from wsad
-where city = 'Redwood City'
-group by 1,2,3,5,7,9,11,13,15,17
+where wsad.city = 'Palo Alto'
+group by 1,2,3,4,6,8,10,12,14,16,18
 union all
 select
        city,
        rok,
-       avg_min_temp_rok,
-       (((avg_min_temp_rok - lag(avg_min_temp_rok) over ())/lag(avg_min_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_min_temp_rok,
-       avg_max_temp_rok,
-       (((avg_max_temp_rok - lag(avg_max_temp_rok) over ())/lag(avg_max_temp_rok) over ())*100)::numeric(3)  yoy_proc_avg_max_temp_rok,
-       avg_mean_temp_rok,
-       (((avg_mean_temp_rok - lag(avg_mean_temp_rok) over ())/lag(avg_mean_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_temp_rok,
-       avg_min_humidity_rok,
-       (((avg_min_humidity_rok - lag(avg_min_humidity_rok) over ())/lag(avg_min_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_min_humidity_rok,
-       avg_max_humidity_rok,
-       (((avg_max_humidity_rok - lag(avg_max_humidity_rok) over ())/lag(avg_max_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_max_humidity_rok,
-       avg_mean_humidity_rok,
-       (((avg_mean_humidity_rok - lag(avg_mean_humidity_rok) over ())/lag(avg_mean_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_humidity_rok,
-       avg_mean_wind_speed_mph_rok,
-       (((avg_mean_wind_speed_mph_rok - lag(avg_mean_wind_speed_mph_rok) over ())/lag(avg_mean_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_wind_speed_mph_rok,
-       avg_max_wind_speed_mph_rok,
-       (((avg_max_wind_speed_mph_rok - lag(avg_max_wind_speed_mph_rok) over ())/lag(avg_max_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_max_wind_speed_mph_rok
+       miesiac,
+       avg_min_temp_month,
+       (((avg_min_temp_month - lag(avg_min_temp_month) over ())/lag(avg_min_temp_month) over ())*100)::numeric(3) MoM_proc_avg_min_temp_month,
+       avg_max_temp_month,
+       (((avg_max_temp_month - lag(avg_max_temp_month) over ())/lag(avg_max_temp_month) over ())*100)::numeric(3)  MoM_proc_avg_max_temp_month,
+       avg_mean_temp_month,
+       (((avg_mean_temp_month - lag(avg_mean_temp_month) over ())/lag(avg_mean_temp_month) over ())*100)::numeric(3) MoM_proc_avg_mean_temp_month,
+       avg_min_humidity_month,
+       (((avg_min_humidity_month - lag(avg_min_humidity_month) over ())/lag(avg_min_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_min_humidity_month,
+       avg_max_humidity_month,
+       (((avg_max_humidity_month - lag(avg_max_humidity_month) over ())/lag(avg_max_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_max_humidity_month,
+       avg_mean_humidity_month,
+       (((avg_mean_humidity_month - lag(avg_mean_humidity_month) over ())/lag(avg_mean_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_mean_humidity_month,
+       avg_mean_wind_speed_mph,
+       (((avg_mean_wind_speed_mph - lag(avg_mean_wind_speed_mph) over ())/lag(avg_mean_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_mean_wind_speed_mph,
+       avg_max_wind_speed_mph,
+       (((avg_max_wind_speed_mph - lag(avg_max_wind_speed_mph) over ())/lag(avg_max_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_max_wind_speed_mph
 
 from wsad
-where city = 'San Jose'
-group by 1,2,3,5,7,9,11,13,15,17
+where wsad.city = 'Redwood City'
+group by 1,2,3,4,6,8,10,12,14,16,18
 union all
 select
        city,
        rok,
-       avg_min_temp_rok,
-       (((avg_min_temp_rok - lag(avg_min_temp_rok) over ())/lag(avg_min_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_min_temp_rok,
-       avg_max_temp_rok,
-       (((avg_max_temp_rok - lag(avg_max_temp_rok) over ())/lag(avg_max_temp_rok) over ())*100)::numeric(3)  yoy_proc_avg_max_temp_rok,
-       avg_mean_temp_rok,
-       (((avg_mean_temp_rok - lag(avg_mean_temp_rok) over ())/lag(avg_mean_temp_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_temp_rok,
-       avg_min_humidity_rok,
-       (((avg_min_humidity_rok - lag(avg_min_humidity_rok) over ())/lag(avg_min_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_min_humidity_rok,
-       avg_max_humidity_rok,
-       (((avg_max_humidity_rok - lag(avg_max_humidity_rok) over ())/lag(avg_max_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_max_humidity_rok,
-       avg_mean_humidity_rok,
-       (((avg_mean_humidity_rok - lag(avg_mean_humidity_rok) over ())/lag(avg_mean_humidity_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_humidity_rok,
-       avg_mean_wind_speed_mph_rok,
-       (((avg_mean_wind_speed_mph_rok - lag(avg_mean_wind_speed_mph_rok) over ())/lag(avg_mean_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_mean_wind_speed_mph_rok,
-       avg_max_wind_speed_mph_rok,
-       (((avg_max_wind_speed_mph_rok - lag(avg_max_wind_speed_mph_rok) over ())/lag(avg_max_wind_speed_mph_rok) over ())*100)::numeric(3) yoy_proc_avg_max_wind_speed_mph_rok
+       miesiac,
+       avg_min_temp_month,
+       (((avg_min_temp_month - lag(avg_min_temp_month) over ())/lag(avg_min_temp_month) over ())*100)::numeric(3) MoM_proc_avg_min_temp_month,
+       avg_max_temp_month,
+       (((avg_max_temp_month - lag(avg_max_temp_month) over ())/lag(avg_max_temp_month) over ())*100)::numeric(3)  MoM_proc_avg_max_temp_month,
+       avg_mean_temp_month,
+       (((avg_mean_temp_month - lag(avg_mean_temp_month) over ())/lag(avg_mean_temp_month) over ())*100)::numeric(3) MoM_proc_avg_mean_temp_month,
+       avg_min_humidity_month,
+       (((avg_min_humidity_month - lag(avg_min_humidity_month) over ())/lag(avg_min_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_min_humidity_month,
+       avg_max_humidity_month,
+       (((avg_max_humidity_month - lag(avg_max_humidity_month) over ())/lag(avg_max_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_max_humidity_month,
+       avg_mean_humidity_month,
+       (((avg_mean_humidity_month - lag(avg_mean_humidity_month) over ())/lag(avg_mean_humidity_month) over ())*100)::numeric(3) MoM_proc_avg_mean_humidity_month,
+       avg_mean_wind_speed_mph,
+       (((avg_mean_wind_speed_mph - lag(avg_mean_wind_speed_mph) over ())/lag(avg_mean_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_mean_wind_speed_mph,
+       avg_max_wind_speed_mph,
+       (((avg_max_wind_speed_mph - lag(avg_max_wind_speed_mph) over ())/lag(avg_max_wind_speed_mph) over ())*100)::numeric(3) MoM_proc_avg_max_wind_speed_mph
 
 from wsad
-where city = 'San Francisco'
-group by 1,2,3,5,7,9,11,13,15,17
+where wsad.city = 'San Jose'
+group by 1,2,3,4,6,8,10,12,14,16,18
