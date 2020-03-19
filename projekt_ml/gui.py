93cf392dd37e39458151878a279555e35f62d812
bartosz-stasiak <b.stasiak@onet.eu>
2020-03-19 20:59:26 +0100

    gui.py developing, v0.01

--- projekt_ml/gui.py
+++ projekt_ml/gui.py
@@ -5,31 +5,50 @@ from ttkthemes import themed_tk as tk
 import tkinter.messagebox
 
 
+####### pomocnicze zmienne listy itp #####################
+
+
+kursy_walut = {'SGD': 0.7218272535233189, 'EUR': 1.1238612937944434, 'DKK': 0.14862087297218637, 'CAD': 0.8145423808817589, 'AUD': 0.8029694136890776, 'GBP': 1.5207327949927543, 'USD': 1.0, 'MXN': 0.05303800288397823, 'HKD': 0.12838130718608456, 'CHF': 1.0320771710383756, 'JPY': 0.008826237200036304, 'SEK': 0.12031274002718356, 'NZD': 0.7616139550831179, 'NOK': 0.12166770926093867, 'PLN': 0.3125, 'Specify goal in USD':1}
+
+kraj_waluta = {'not considered': 'Specify goal in USD','Poland':'PLN','Other':'Specify goal in USD','Singapore': 'SGD', 'Ireland': 'EUR', 'Norway': 'NOK', 'New Zealand': 'NZD', 'Netherlands': 'EUR', 'Australia': 'AUD', 'Spain': 'EUR', 'Mexico': 'MXN', 'Hong Kong': 'HKD', 'United States': 'USD', 'Denmark': 'DKK', 'Japan': 'JPY', 'Luxembourg': 'EUR', 'Italy': 'EUR', 'Germany': 'EUR', 'Canada': 'CAD', 'Sweden': 'SEK', 'Great Britain (UK)': 'GBP', 'Belgium': 'EUR', 'Austria': 'EUR', 'Switzerland': 'CHF', 'France': 'EUR'}
 
 main_chosen = ''
 
+
+
+
+
+############# definicje fukcji ##########
+
+
+# pomocnicza z kursu tkinter
 def doNothing():
     print("ok ok I won't...")
 
 
+
+# sprawdzenie i użycie testowe wybranych w widźetach parametrów
 def checkParamas():
     tmpCAT = catMenu.get()
     tmpCNTR = countryMenu.get()
+    tmpSUBCAT = subcatMenu.get()
+    tmpGOAL = goalSpinbox.get()
+    localCURR = kraj_waluta[tmpCNTR]
+    localGOAL = round(float(tmpGOAL)/kursy_walut[localCURR],1)
     global main_chosen
     main_chosen = tmpCAT
-    sample_label.configure(text = f'you chose main cat: {tmpCAT} , and country: {tmpCNTR}')
-    narrow_subcat(tmpCAT)
+    paramInfolabel.configure(text = f"Your Campaign's main category: {tmpCAT}\n "
+                                    f"Your Campaing's sub category : {tmpSUBCAT}\n"
+                                    f"Your Campaign's launch country: {tmpCNTR}\n"
+                                    f"Your Campaign's goal is {tmpGOAL} USD ({localGOAL} in {localCURR} ).")
 
-
+#pomocnicza funkcja sprawdzająca czy została zapisana wartość globalnej zmiennej main_chosen
 def run_magic():
     print(main_chosen)
 
 
-# o tutaj jest funkcja, która wykorzystuje słownik zbudowany na zasadzie {główne kategorie: adekwatne podkategorie},
-# żeby ustawić w dropdownie odpowiednie podkategorie
-# (to jest ogólnie de facto tak, że tworzymy zmienne typu widget, które mają odpowiednie parametry)
 
-
+######### lista podkategorii do wyboru dla poszczególnych wybrbanych main_category
 sub_categories = {'Art':
                           ['Sculpture',
                            'Conceptual Art',
@@ -220,13 +239,11 @@ sub_categories = {'Art':
                           ['choose main category first']
                            }
 
-def narrow_subcat(maincat):
 
-    subcatMenu.values = list(sub_categories[maincat])
-
-
-
-
+#funkcja, która daje listę podkategorii do wyboru na podstawie wybranej kategorii
+def narrow_subcat(event):
+    tmpCAT = catMenu.get()
+    subcatMenu.configure(values = sub_categories[tmpCAT])
 
 
 
@@ -235,25 +252,44 @@ def narrow_subcat(maincat):
 
 
 
-# ***** GUI strt *****
+
+################### definicja interfesju ############
+'''
+Tkinter opiera się o "ramki" - frames, w których upakowuje się poszczególne fragmentu interfejsu:
+- przyciski
+- pola tekstowe
+- menu wybieralne
+- slidery 
+- okienka inputu 
+i pewnie jeszce wiele innych
+
+ramek może być wiele poziomów, sam stosuje poniżej "ramki w większych ramkach"
+
+'''
 
 
+
+# ***** GUI start *****
+
+
+#root - główna, najbardziej zewnętrzna ramka, definiowana jako klasa ThemedTk (themed, zeby skorzystać z predefiniowanego styu)
+# "okno programu, nazwa root wzięta z kursu"
 
 root = tk.ThemedTk()
 root.geometry('1000x600')
 root.set_theme("plastik")
 
 
-# cols = 4
-# rows = 4
 
 
+# ***** The Toolbar - górny pasek z przyciakami  *****
 
-# ***** The Toolbar *****
-
+#pierwsza zewnętrzna ramka, umieszczamy ją w oknie 'root', "pakujemy" na górze
 toolbar = ttk.Frame(root)
 toolbar.pack(side = TOP)
 
+#### poniżej definicja przycików używanych, które "pakujemy" w w ramce "toolbar"
+#### w dalszej części na podobnej zasadzie są zdefiniowane i "upakowane" pozostałe elementy interfejsu
 
 # *** toolbar buttons *****
 insertButt = ttk.Button(toolbar, text="check main category", command=checkParamas)
@@ -272,12 +308,15 @@ quitButt.pack(side=RIGHT, padx=10, pady=10)
 
 # ***** Menus Frame *****
 
+
 menuFrame = Frame(root)
 menuFrame.configure(padx = 30,pady = 30)
 
 
 menuFrame.pack(side = TOP)
 
+MenusLabel1 = ttk.Label(menuFrame, text = 'Please specify campaign parameters    ', anchor = CENTER )
+MenusLabel1.pack(side = TOP)
 
 
 #** Main Category selector *****
@@ -300,10 +339,11 @@ mcats = ['Art',
              'Theater',
              'Games']
 
-catMenuLabel = ttk.Label(catMenuFrame, text = 'Choose Main Category    ', anchor = N )
+catMenuLabel = ttk.Label(catMenuFrame, text = 'Choose main category    ', anchor = N )
 catMenuLabel.pack(side = LEFT)
 
 catMenu = ttk.Combobox(catMenuFrame, values = mcats)
+catMenu.bind('<<ComboboxSelected>>', narrow_subcat )
 catMenu.pack(side = LEFT)
 
 catMenuFrame.pack(side = TOP)
@@ -358,6 +398,30 @@ countryMenu.pack(side = LEFT)
 countryMenuFrame.pack(side = TOP)
 
 
+############# nad tym aktualnie pracuję, żeby był ładny slider połączony z okiekiem do wpisywania
+
+# ** Goal selector *****
+goalMenuFrame = Frame(menuFrame, padx = 2, pady = 2)
+
+
+goalMenuLabel = ttk.Label(goalMenuFrame, text = 'Set Goal in USD         ' )
+goalMenuLabel.pack(side = LEFT)
+
+#goalSlider = ttk.Scrollbar(goalMenuFrame, orient = HORIZONTAL)
+goalSpinbox = ttk.Spinbox(goalMenuFrame, from_ = 1, to = 20000000, command = checkParamas)
+goalButton = Button(goalMenuFrame, text="OK", command=checkParamas)
+
+
+
+#goalSlider.configure(command = goalSpinbox.set)
+
+goalSpinbox.pack(side = LEFT)
+#goalSlider.pack(side = BOTTOM, fill = X)
+goalButton.pack(side = LEFT)
+
+goalMenuFrame.pack(side = LEFT)
+
+
 
 # ***** Main Window showing result *****
 
@@ -367,10 +431,9 @@ mainFrame1.pack(side = TOP)
 
 
 
-sample_label = ttk.Label(mainFrame1, text = 'choose main cat')
+paramInfolabel = ttk.Label(mainFrame1, text = 'choose main cat')
 
-sample_label.pack(side = TOP, anchor = CENTER, fill = BOTH)
-
+paramInfolabel.pack(side = TOP, anchor = CENTER, fill = BOTH)
 
 
 
@@ -378,9 +441,8 @@ sample_label.pack(side = TOP, anchor = CENTER, fill = BOTH)
 
 status = ttk.Label(root, text="PandP, ML_project, v0.01", relief=GROOVE, anchor=W)
 
-#status.grid(row = rows, columnspan = cols, sticky = S)
 status.pack(side=BOTTOM, fill=X)
 
 
-
+# "włączenie" programu, interfesj musi się zawierać pomiędzy "otwarciem" roota i jego "mainloop'em", trochę jak w html'u
 root.mainloop()
